d019a34728d43420a33ab9059d1d6b36b9cbb44f
Tom Harkness <tharkness100@gmail.com>
2016-03-18 14:13:55 -0700

    game added to front page

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,75 @@
-// Initialize Phaser, and creates a 400x490px game
-var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game_div');
-var game_state = {};
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-// Creates a new 'main' state that wil contain the game
-game_state.main = function() { };
-game_state.main.prototype = {
+var score = 0;
+var scoreText;
 
-    create: function() {
-    	// Fuction called after 'preload' to setup the game
-        this.hello_sprite = game.add.sprite(250, 300, 'hello');
-    },
+function preload() {
+  game.load.tilemap('testMap', 'assets/testmap.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.image('test_map', 'assets/test_map.png');
+  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+}
 
-    update: function() {
-		// Function called 60 times per second
-        this.hello_sprite.angle += 1;
-    },
-};
+function create() {
+  game.physics.startSystem(Phaser.Physics.P2JS);
 
-// Add and start the 'main' state to start the game
-game.state.add('main', game_state.main);
-game.state.start('main');
+  game.physics.p2.setImpactEvents(true);
+
+  game.add.sprite(0, 0, 'star');
+
+  // map
+  map = game.add.tilemap('testMap');
+  map.addTilesetImage('test_map');
+  
+  background = map.createLayer('Tile Layer 1');
+  backgroundOL = map.createLayer('overlays');
+  collisionLayer = map.createLayer('collision');
+  map.setCollision([13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27], true, 'collision');
+  game.physics.p2.convertTilemap(map, 'collision');
+
+
+  //player
+  player = game.add.sprite(32, game.world.height - 150, 'dude');
+  game.physics.p2.enable(player);
+  player.body.collideWorldBounds = true;
+  player.body.fixedRotation = true;
+  player.body.clearShapes();
+  player.body.addRectangle(48, 48, 0, 0);
+  //player animations    name      frames    fps  loop or not to loop
+  player.animations.add('left', [0, 1, 2, 3], 10, true);
+  player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+  // score tracking
+  scoreText = game.add.text( 16, 16, 'score: 0', {fontSize: '32px', fill: '#000'});
+}
+
+function update() {
+
+  // player controls
+  cursors = game.input.keyboard.createCursorKeys();
+  player.body.velocity.x = 0;
+  // player walk
+  if (cursors.left.isDown) {
+    player.body.moveLeft(200);
+    player.animations.play('left');
+  } else if (cursors.right.isDown) {
+    player.body.moveRight(200);
+    player.animations.play('right');
+  } else {
+    player.animations.stop();
+    player.body.velocity.x = 0;
+    player.frame = 4;
+  }
+
+  if (cursors.up.isDown) {
+    player.body.moveUp(200);
+    player.animations.play('left');
+  } else if (cursors.down.isDown) {
+    player.body.moveDown(200);
+    player.animations.play('left');
+  } else {
+    player.animations.stop();
+    player.body.velocity.y = 0;
+    player.frame = 4;
+  }
+
+}
