7a210a123fd50b2a595c607e78481c9092867965
Reinhard Simeon <reinhardsimeon@gmail.com>
2016-03-20 22:23:41 +0000

    initial individual bar

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,7 @@ game_state.main.prototype = {
         var playerHealth = 100;
         var testHealth = 100;
         var hpBar1;
+        var graphics;
 
         //Health bar
         var barConfig = {
@@ -24,19 +25,44 @@ game_state.main.prototype = {
 
         hpBar1 = new HealthBar(this.game, barConfig);
         hpBar1.setFixedToCamera(true);
+
+        // draw graphics
+        graphics = this.game.add.graphics(0,0);
+        // set a fill and line style
+        graphics.beginFill(0x00FF00);
+        graphics.drawRect(0, 0, 50, 10);
+        console.log(graphics.width);
+        player.addChild(graphics);
     },
 
     update: function() {
 		// Function called 60 times per second
         this.hello_sprite.angle += 1;
 
+        //decrease aggreagate bar
         damageHealth(hpBar1);
+        //decrease individual bar
+        damageGraph(setWidth(testHealth));
     },
 
     function damageHealth(bar){
       playerHealth -= 10;
       if(playerHealth < 0) playerHealth = 0;
       bar.setPercent(playerHealth);
+    }, 
+
+    function damageGraph(newWidth){
+      game.add.tween(graphics).to( { width: newWidth }, 200, Phaser.Easing.Linear.None, true);
+    }
+
+    function setWidth(newValue){
+      if(newValue < 0) newValue = 0;
+      if(newValue > 100) newValue = 100;
+      console.log("The graphics width is:" + graphics.width);
+      //TODO: DO NOT HARD CODE - get the individual width
+      newWidth = (newValue * 50) / 100;
+      console.log("The new width: " + newWidth);
+      return newWidth;
     }
 };
 
