52f552c5da9fcd77611f874c90afe6bd1d7432c0
djtouchette <djtouchette1993@gmail.com>
2016-03-24 15:05:44 -0700

    Assigned stats, along with x,y.

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -36,11 +36,18 @@ socket.on('waiting', function () {
 });
 
 socket.on ('spriteClass', function (unit) {
+  var UnitArr = allUnits[unit.index];
   // unit.index = unit.index -1;
   // console.log('this is turn:' + turn);
   console.log(unit);
-  allUnits[unit.index].x = unit.x;
-  allUnits[unit.index].y = unit.y;
+
+  UnitArr.x = unit.x;
+  UnitArr.y = unit.y;
+  UnitArr.atk = unit.atk;
+  UnitArr.def = unit.def;
+  UnitArr.spd = unit.spd;
+  UnitArr.troops = unit.troops;
+
   turnSwitch = true;
 
 
